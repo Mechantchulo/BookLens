bd6a03c5c49e38e0a35ba57362d9ee0daba336d1
Mechantchulo <chulomechant@gmail.com>
2025-07-12 17:31:37 +0300

    Comparison button added

--- components/BookCard.jsx
+++ components/BookCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BookCard = ({ book }) => {
+const BookCard = ({ book, handleCompareClick, isSelected }) => {
   const { title, authors, publishedDate } = book.volumeInfo;
 
   return (
@@ -8,6 +8,9 @@ const BookCard = ({ book }) => {
       <h2>{title}</h2>
       <p>{authors ? authors.join(", ") : "Unknown Author"}</p>
       <p>{publishedDate ? publishedDate.slice(0, 4) : "Unknown Date"}</p>
+      <button onClick={() => handleCompareClick(book)}>
+        {isSelected ? "✓ Selected" : "compare"}
+      </button>
     </div>
   );
 };

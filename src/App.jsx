a6c6e37574b8042cf60d1a8072add4d201830036
Mechantchulo <chulomechant@gmail.com>
2025-07-12 17:12:04 +0300

    App.jsx changes

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Search from "../components/Search";
+import axios from "axios";
+import BookCard from "../components/BookCard";
+import BookList from "../components/BookList";
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(false);
+
+  const onSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const onSearchSubmit = async () => {
+    if (!searchTerm.trim()) {
+      return;
+    }
+
+    const options = {
+      method: "GET",
+      url: "https://www.googleapis.com/books/v1/volumes",
+      params: {
+        q: searchTerm,
+        maxResults: 20,
+        key: import.meta.env.VITE_BOOKS_API_KEY,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setBooks(response.data.items);
+    } catch (error) {
+      console.error(error);
+      setErrorMsg(`Trouble finding your book`);
+    }
+  };
+  return (
+    <div>
+      <Search
+        searchTerm={searchTerm}
+        onSearchChange={onSearchChange}
+        onSearchSubmit={onSearchSubmit}
+      />
+
+      <BookList books={books} />
+    </div>
+  );
+};
+
+export default App;

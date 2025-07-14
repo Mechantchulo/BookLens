7472fd7db48485021cba4c5d6218c0a5fde3ebbd
Mechantchulo <chulomechant@gmail.com>
2025-07-14 23:23:41 +0300

    Updated comparison logic

--- components/BookList.jsx
+++ components/BookList.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import BookCard from "./BookCard";
 
-const BookList = ({ books }) => {
+const BookList = ({ books, selectedBooks, handleCompareClick }) => {
   if (!books || books.length === 0) {
     return <p className="text-center text-gray-500 mt-4">No books found.</p>;
   }
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      {books.map((book) => {
-        return <BookCard key={book.id} book={book} />;
-      })}
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          book={book}
+          handleCompareClick={handleCompareClick}
+          isSelected={selectedBooks.some((b) => b.id === book.id)}
+        />
+      ))}
     </div>
   );
 };
